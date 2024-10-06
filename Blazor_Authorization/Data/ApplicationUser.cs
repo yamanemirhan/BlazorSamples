8cde613f72b999197c6b2e42e610bb5cf6c29d79
Emirhan Yaman <yaman.emrhn@gmail.com>
2024-10-06 23:47:45 +0300

    role-based authorization with identity

---
+++ Blazor_Authorization/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Blazor_Authorization.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+}
---
+++ Blazor_Authorization/Data/Migrations/20241006142237_IdentityRoleAdded.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blazor_Authorization.Migrations
+{
+    /// <inheritdoc />
+    public partial class IdentityRoleAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
